6e826147f7e88fb0a9fca16a946764268c149a25
Rubel Hossain <rubrex96@hotmail.com>
2022-04-26 01:02:11 +0600

    comment

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -36,6 +36,7 @@ export const fetchSlider = (sliderValue = 1) => dispatch => {
     const currentEPOCH = Math.floor(Date.now() / 1000);
     // in perm Slider value must be inserted after 86400
     const fromEPOCH = currentEPOCH - (86400 * sliderValue);
+    // save post JSON format
     const postEPOCH = { "fromdate": fromEPOCH, "todate": currentEPOCH }
     console.log(postEPOCH);
     axios.post(GRAPHDATA_API, postEPOCH)
